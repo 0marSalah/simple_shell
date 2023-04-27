f021179acf3ff7b21c5deb3584c8e265915faa33
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-04-27 21:54:07 +0200

    string functions

---
+++ _strncpy.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+char *s = dest;
+int i, j;
+
+i = 0;
+while (src[i] != '\0' && i < n - 1)
+{
+dest[i] = src[i];
+i++;
+}
+if (i < n)
+{
+j = i;
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+}
+return (s);
+}
