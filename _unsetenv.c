941086c18edb73d88fc095563fff7f6b71844353
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-04-28 20:51:05 +0300

    unset environment

--- _unsetenv.c
+++ _unsetenv.c
@@ -1,27 +1,6 @@
 #include "shell.h"
 
 /**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *		constant function prototype.
- *  Return: Always 0
- */
-int _myunsetenv(info_t *info)
-{
-	int i;
-
-	if (info->argc == 1)
-	{
-		_error_puts("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
-
-	return (0);
-}
-
-/**
  * _unsetenv - Remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *		constant function prototype.
